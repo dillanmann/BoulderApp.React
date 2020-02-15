3876aa917b7b120ddaa15bab3c40e6e6bb81c766
Dillanm <dillan.mann@gmail.com>
2020-02-15 09:24:23 +0000

    Updates

---
+++ precache-manifest.4ea72c7d94dbee2192b4aeb7a09976cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fbe705b0ad4cd1058226086a660aba",
+    "url": "/BoulderApp.React/index.html"
+  },
+  {
+    "revision": "e32d26f198cf0151372f",
+    "url": "/BoulderApp.React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e32d26f198cf0151372f",
+    "url": "/BoulderApp.React/static/js/2.e87452ae.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/BoulderApp.React/static/js/2.e87452ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88915f2c592f6fc96144",
+    "url": "/BoulderApp.React/static/js/main.470abeb8.chunk.js"
+  },
+  {
+    "revision": "10553bb975c72aae8ee1",
+    "url": "/BoulderApp.React/static/js/runtime-main.1906aef4.js"
+  }
+]);
